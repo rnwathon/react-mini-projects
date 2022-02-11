8a7774dfc97bc9f63410ec75f5c6a9652a7ca85b
rnwathon <rakhmatnashrul@gmail.com>
2022-02-12 03:03:45 +0700

    feat(global): create basic header component

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-[#282c34] text-[#61dafb] h-[300px] flex justify-center items-center mb-5">
+      <h1 className="text-xl font-bold tracking-widest">React Mini Projects</h1>
+    </header>
+  );
+}
+
+export default Header;
