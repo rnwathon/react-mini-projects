e7bdb78507e26e3ff53079bb4203fbc834a50d9b
rnwathon <rakhmatnashrul@gmail.com>
2022-02-13 00:05:40 +0700

    chore: configure react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import * as React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/0-Home';
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
