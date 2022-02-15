5be3ed0764fc906e4a396bc1cbe60fcf22385fe4
rnwathon <rakhmatnashrul@gmail.com>
2022-02-15 23:28:50 +0700

    feat: use state and clear button in person list page

--- src/Pages/1-PersonList/index.jsx
+++ src/Pages/1-PersonList/index.jsx
@@ -3,12 +3,17 @@ import data from './data';
 import List from './List';
 
 function PersonList() {
+  const [persons, setPersons] = React.useState(data);
   return (
     <section className="bg-slate-100 h-screen flex items-center justify-center">
       <section className="bg-white shadow-lg rounded-lg w-96 py-5 px-3">
         <h1 className="text-lg text-center text-red-400 font-bold"> Person List </h1>
-        <List list={data} />
-        <button type="button" className="bg-red-400 w-full p-3 rounded-lg text-white">
+        <List list={persons} />
+        <button
+          type="button"
+          className="bg-red-400 w-full p-3 rounded-lg text-white"
+          onClick={() => setPersons([])}
+        >
           Clear List
         </button>
       </section>
