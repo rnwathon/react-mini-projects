8721cb7ce09256215dbb54ee47b161a98abf6b92
rnwathon <rakhmatnashrul@gmail.com>
2022-02-19 03:37:20 +0700

    feat: change Accordion component into compound component & add show one content logic

--- src/Pages/3-FAQ/Components/Accordion.jsx
+++ src/Pages/3-FAQ/Components/Accordion.jsx
@@ -2,35 +2,85 @@ import clsx from 'clsx';
 import * as React from 'react';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
-function Accordion(props) {
-  const { info, title } = props;
-  const [showDetail, setShowDetail] = React.useState(false);
+const AccordionContext = React.createContext();
 
-  const style = {
-    wrapper: clsx(
-      'rounded-lg w-full mb-4',
-      'ease-in-out duration-500',
-      showDetail ? 'shadow-xl' : 'shadow-md'
-    ),
-    content: clsx(
-      'px-3',
-      'overflow-hidden',
-      'ease-in-out duration-500',
-      showDetail ? 'max-h-[500px] py-3' : 'max-h-0'
-    ),
-  };
+const Accordion = ({ children }) => {
+  const [activeContent, setActiveContent] = React.useState(null);
+
+  const toggleShowContent = React.useCallback(
+    (itemId) => {
+      if (itemId === activeContent) return setActiveContent(null);
+
+      setActiveContent(itemId);
+    },
+    [activeContent]
+  );
 
   return (
-    <div className={style.wrapper}>
-      <header className="flex justify-between p-3">
-        <h2 className="font-bold">{title}</h2>
-        <button type="button" onClick={() => setShowDetail(!showDetail)}>
-          {showDetail ? <FaChevronUp /> : <FaChevronDown />}
-        </button>
-      </header>
-      <article className={style.content}>{info}</article>
-    </div>
+    <AccordionContext.Provider
+      value={React.useMemo(
+        () => ({ activeContent, toggleShowContent }),
+        [activeContent, toggleShowContent]
+      )}
+    >
+      {children}
+    </AccordionContext.Provider>
   );
-}
+};
+
+const AccordionItemContext = React.createContext();
+
+const AccordionItem = ({ children, id }) => {
+  const style = clsx(
+    'rounded-lg w-full mb-4',
+    'ease-in-out duration-500',
+    'shadow-md border border-slate-100'
+  );
+
+  return (
+    <AccordionItemContext.Provider value={React.useMemo(() => ({ id }), [id])}>
+      <div className={style}>{children}</div>
+    </AccordionItemContext.Provider>
+  );
+};
+
+const Header = ({ children }) => {
+  const { toggleShowContent, activeContent } = React.useContext(AccordionContext);
+  const { id } = React.useContext(AccordionItemContext);
+
+  const style = clsx('flex justify-between p-3 cursor-pointer');
+  return (
+    <header className={style} onClick={() => toggleShowContent(id)} aria-hidden="true">
+      <h2 className="font-bold">{children}</h2>
+      <button type="button">
+        {activeContent === id ? <FaChevronUp /> : <FaChevronDown />}
+      </button>
+    </header>
+  );
+};
+
+const Content = ({ children }) => {
+  const { activeContent } = React.useContext(AccordionContext);
+  const { id } = React.useContext(AccordionItemContext);
+  const contentEl = React.useRef(null);
+
+  const style = clsx('overflow-hidden transition-all duration-500 px-3');
+
+  return (
+    <article
+      ref={contentEl}
+      className={style}
+      style={{
+        maxHeight: activeContent === id ? `${contentEl?.current?.scrollHeight}px` : 0,
+      }}
+    >
+      {children}
+    </article>
+  );
+};
+
+Accordion.Item = AccordionItem;
+Accordion.ItemHeader = Header;
+Accordion.ItemContent = Content;
 
 export default Accordion;
--- src/Pages/3-FAQ/index.jsx
+++ src/Pages/3-FAQ/index.jsx
@@ -6,13 +6,20 @@ function FAQ() {
   const [questions, setQuestions] = React.useState(data);
 
   return (
-    <div className="bg-red-400 h-screen flex flex-col items-center justify-center p-5">
+    <div className="bg-red-400 h-screen flex flex-col items-center p-5">
       <div className="bg-white w-full sm:w-2/3 md:w-1/2 rounded-xl p-5 flex gap-2">
         <h1 className="text-3xl w-1/3">Frequently Asked Question</h1>
         <div className="w-2/3">
-          {questions.map((question) => (
-            <Accordion key={question.id} {...question} />
-          ))}
+          <Accordion>
+            {questions.map((question) => (
+              <Accordion.Item key={question.id} id={question.id}>
+                <Accordion.ItemHeader>{question.title}</Accordion.ItemHeader>
+                <Accordion.ItemContent>
+                  <p className="my-3">{question.info}</p>
+                </Accordion.ItemContent>
+              </Accordion.Item>
+            ))}
+          </Accordion>
         </div>
       </div>
     </div>
