9aa06e245ffc7676acac47f0cafdb0914832a2bf
rnwathon <rakhmatnashrul@gmail.com>
2022-02-19 01:56:28 +0700

    feat: add animation to accordion component

--- src/Pages/3-FAQ/Components/Accordion.jsx
+++ src/Pages/3-FAQ/Components/Accordion.jsx
@@ -1,22 +1,34 @@
+import clsx from 'clsx';
 import * as React from 'react';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 function Accordion(props) {
   const { info, title } = props;
   const [showDetail, setShowDetail] = React.useState(false);
+
+  const style = {
+    wrapper: clsx(
+      'rounded-lg w-full mb-4',
+      'ease-in-out duration-500',
+      showDetail ? 'shadow-xl' : 'shadow-md'
+    ),
+    content: clsx(
+      'px-3',
+      'overflow-hidden',
+      'ease-in-out duration-500',
+      showDetail ? 'max-h-[500px] py-3' : 'max-h-0'
+    ),
+  };
+
   return (
-    <div
-      className={`rounded-lg w-full mb-4 transition-all ${
-        showDetail ? 'shadow-xl' : 'shadow-md'
-      }`}
-    >
+    <div className={style.wrapper}>
       <header className="flex justify-between p-3">
         <h2 className="font-bold">{title}</h2>
         <button type="button" onClick={() => setShowDetail(!showDetail)}>
           {showDetail ? <FaChevronUp /> : <FaChevronDown />}
         </button>
       </header>
-      {showDetail && <article className="p-3">{info}</article>}
+      <article className={style.content}>{info}</article>
     </div>
   );
 }
