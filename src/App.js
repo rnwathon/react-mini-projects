359cb90578f302a4204ff6456573186b9ffd34ff
rnwathon <rakhmatnashrul@gmail.com>
2022-02-20 16:17:42 +0700

    refactor: change FAQ project to number 4 in the list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/0-Home';
 import PersonList from './Pages/1-PersonList';
 import RandomQuote from './Pages/2-RandomQuote';
-import FAQ from './Pages/3-FAQ';
+import Carousel from './Pages/3-Carousel';
+import FAQ from './Pages/4-FAQ';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/1-PersonList" element={<PersonList />} />
       <Route path="/2-RandomQuote" element={<RandomQuote />} />
-      <Route path="/3-FAQ" element={<FAQ />} />
+      <Route path="/3-Carousel" element={<Carousel />} />
+      <Route path="/4-FAQ" element={<FAQ />} />
     </Routes>
   );
 }
--- src/Pages/4-FAQ/index.jsx
+++ src/Pages/4-FAQ/index.jsx
@@ -6,7 +6,7 @@ function FAQ() {
   const [questions, setQuestions] = React.useState(data);
 
   return (
-    <div className="bg-red-400 h-screen flex flex-col items-center p-5">
+    <div className="bg-blue-400 h-screen flex flex-col items-center p-5">
       <div className="bg-white w-full sm:w-2/3 md:w-1/2 rounded-xl p-5 flex gap-2">
         <h1 className="text-3xl w-1/3">Frequently Asked Question</h1>
         <div className="w-2/3">
