e8c5819ae958ab1e63f86cf797a770910684db32
rnwathon <rakhmatnashrul@gmail.com>
2022-03-03 00:05:22 +0700

    feat: add loading text when requesting the quote in RandomQuote

--- src/Pages/2-RandomQuote/index.jsx
+++ src/Pages/2-RandomQuote/index.jsx
@@ -27,10 +27,14 @@ function RandomQuote() {
     <div className="bg-red-400 h-screen flex flex-col items-center justify-center p-5">
       <p className="text-center text-xl text-stone-900/50 mb-4 font-bold">Random Quote</p>
       <div className="w-full md:w-2/3 lg:w-1/3 min-h-[300px] bg-white rounded-xl shadow-lg px-5 pt-5 pb-20 relative">
-        <div>
-          <Quote quote={quote} />
-          <Author author={author} />
-        </div>
+        {!quote || !author ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <Quote quote={quote} />
+            <Author author={author} />
+          </div>
+        )}
         <button
           type="button"
           className="bg-red-500 text-white py-2 px-5 rounded-xl shadow-lg shadow-red-500/40 absolute left-5 bottom-5"
