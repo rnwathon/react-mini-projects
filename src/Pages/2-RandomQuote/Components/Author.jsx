479a3037605f56a2b2595d37ce395cd306c01cdf
rnwathon <rakhmatnashrul@gmail.com>
2022-03-03 16:57:09 +0700

    test: add test id to Quote & Author component

--- src/Pages/2-RandomQuote/Components/Author.jsx
+++ src/Pages/2-RandomQuote/Components/Author.jsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 
 function Author(props) {
   const { author } = props;
-  return <div className="text-slate-500 text-xl mb-2">{`- ${author}`}</div>;
+  return (
+    <div
+      className="text-slate-500 text-xl mb-2"
+      data-testid="author"
+    >{`- ${author}`}</div>
+  );
 }
 
 export default Author;
--- src/Pages/2-RandomQuote/Components/Quote.jsx
+++ src/Pages/2-RandomQuote/Components/Quote.jsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 
 function Quote(props) {
   const { quote } = props;
-  return <div className="text-3xl font-serif italic mb-2">{`"${quote}"`}</div>;
+  return (
+    <div
+      className="text-3xl font-serif italic mb-2"
+      data-testid="quote"
+    >{`"${quote}"`}</div>
+  );
 }
 
 export default Quote;
