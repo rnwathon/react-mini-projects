cd03df5ee3fa25ccbda1ff75ff038088b3f353e6
rnwathon <rakhmatnashrul@gmail.com>
2022-03-07 19:07:13 +0700

    feat: add todo list state and create new todo functionality

--- src/Pages/7-TodoList/index.jsx
+++ src/Pages/7-TodoList/index.jsx
@@ -1,6 +1,25 @@
 import * as React from 'react';
 
 function TodoListApp() {
+  const [todos, setTodos] = React.useState([
+    'Grocery date',
+    'Pay bills',
+    'Go to courses',
+    'Summer camp',
+    'Do your bed',
+  ]);
+  const [todoInput, setTodoInput] = React.useState('');
+
+  const handleChange = (e) => {
+    setTodoInput(e.target.value);
+  };
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    setTodos((prevTodos) => [...prevTodos, todoInput]);
+    setTodoInput('');
+  };
+
   return (
     <div className="bg-slate-100 h-screen">
       <header className="bg-green-300 h-80 flex items-center justify-center">
@@ -12,23 +31,20 @@ function TodoListApp() {
             type="text"
             placeholder="What to do?"
             className="grow pl-2 focus:outline-none"
+            value={todoInput}
+            onChange={handleChange}
           />
           <button
             type="submit"
             className="bg-green-500 hover:bg-green-600 px-8 h-full rounded-lg text-white"
+            onClick={handleAddTodo}
           >
             Submit
           </button>
         </section>
         <section className="bg-white rounded-lg shadow-xl min-h-96">
           <ul className="p-5 divide-y">
-            {[
-              'Grocery date',
-              'Pay bills',
-              'Go to courses',
-              'Summer camp',
-              'Do your bed',
-            ].map((todo, idx) => (
+            {todos.map((todo, idx) => (
               // eslint-disable-next-line react/no-array-index-key
               <li key={`todo-${idx}`} className="py-2">
                 <label htmlFor={`todo-${idx}`}>
