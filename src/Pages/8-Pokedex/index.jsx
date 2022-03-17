859f321b54a3ef2ce062db0b5526727ef974c5dc
rnwathon <rakhmatnashrul@gmail.com>
2022-03-17 22:18:38 +0700

    feat(Pokedex): get Pokemon details functionality

--- src/Pages/8-Pokedex/index.jsx
+++ src/Pages/8-Pokedex/index.jsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Button from './Components/Button';
 import Card from './Components/Card';
 import Container from './Components/Container';
+import Modal from './Components/Modal';
+import { useModal } from './Hooks';
 import './index.css';
 
 const API_URL = 'https://pokeapi.co/api/v2';
@@ -9,10 +11,14 @@ const limit = 20;
 
 function Pokedex() {
   const [pokemonList, setPokemonList] = React.useState([]);
+  const [pokemonDetails, setPokemonDetails] = React.useState({});
+  const [pokemonDetailsLoading, setPokemonDetailsLoading] = React.useState(false);
   const [totalPage, setTotalPage] = React.useState();
   const [page, setPage] = React.useState(1);
   const [prevUrl, setPrevUrl] = React.useState();
   const [nextUrl, setNextUrl] = React.useState();
+
+  const [isModalOpen, setIsModalOpen] = useModal(false);
 
   const getPokemonList = React.useCallback((url) => {
     fetch(url)
@@ -25,6 +31,22 @@ function Pokedex() {
         setNextUrl(next);
       });
   }, []);
+
+  const getPokemonDetails = (name) => {
+    setPokemonDetailsLoading(true);
+    fetch(`${API_URL}/pokemon/${name}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { sprites, stats, types } = data;
+        setPokemonDetails({ name, sprites, stats, types });
+        setPokemonDetailsLoading(false);
+      });
+  };
+
+  const handleClickPokemon = (name) => {
+    setIsModalOpen(true);
+    getPokemonDetails(name);
+  };
 
   const handlePrevPage = (e) => {
     e.preventDefault();
@@ -57,6 +79,7 @@ function Pokedex() {
                 <Card
                   key={`pokemon-${idx}`}
                   className="px-4 py-2 text-center cursor-pointer bg-orange-200 hover:bg-orange-300"
+                  onClick={() => handleClickPokemon(pokemon.name)}
                 >
                   {pokemon.name}
                 </Card>
@@ -77,6 +100,27 @@ function Pokedex() {
           </div>
         </div>
       </div>
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <Modal.Header> Pokemon Details </Modal.Header>
+          <Modal.Body>
+            {pokemonDetailsLoading ? (
+              <div className="grid justify-center items-center"> Loading... </div>
+            ) : (
+              <>
+                <div className="flex justify-center">
+                  <img
+                    src={pokemonDetails?.sprites?.front_default}
+                    alt="pokemon sprite"
+                    className="w-36"
+                  />
+                </div>
+                <p className="text-center">{pokemonDetails?.name}</p>
+              </>
+            )}
+          </Modal.Body>
+        </Modal>
+      )}
     </div>
   );
 }
