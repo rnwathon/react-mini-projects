1d8aac58962942447bbb93e9b8d1587582d15a4e
rnwathon <rakhmatnashrul@gmail.com>
2022-04-04 20:12:58 +0700

    feat(TriviaQuiz): add useReducer to hold quiz and answers data

--- src/Pages/9-TriviaQuiz/index.jsx
+++ src/Pages/9-TriviaQuiz/index.jsx
@@ -1,18 +1,67 @@
 import * as React from 'react';
 import Card from './Components/Card';
+import Option from './Components/Option';
+import Question from './Components/Question';
 import { categories } from './Constant';
 
 const API_URL = 'https://opentdb.com/api.php';
 
+const initialState = {
+  quiz: [],
+  answers: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_QUIZ':
+      return { ...state, quiz: action.payload };
+    case 'SET_ANSWER':
+      return { ...state, answers: action.payload };
+    default:
+      return state;
+  }
+};
+
 function TriviaQuiz() {
   const [isStarted, setIsStarted] = React.useState(false);
+  const [activeQuestion, setActiveQuestion] = React.useState();
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const isPrevDisabled = activeQuestion === 0;
+  const isNextDisabled = !state.answers[activeQuestion];
 
   const handleClickCategory = (value) => {
     setIsStarted(true);
     fetch(`${API_URL}/?amount=10&difficulty=easy&type=multiple&category=${value}`)
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        const quizWithOptions = data.results.map((quiz) => ({
+          ...quiz,
+          options: [quiz.correct_answer, ...quiz.incorrect_answers],
+        }));
+
+        dispatch({ type: 'SET_QUIZ', payload: quizWithOptions });
+        setActiveQuestion(0);
+      })
       .catch((err) => console.log(err));
+  };
+
+  const handleChangeOption = (e, idx) => {
+    const currentAnswers = [...state.answers];
+    if (currentAnswers[idx]) {
+      currentAnswers.splice(idx, 1, e.target.value);
+    } else {
+      currentAnswers[idx] = e.target.value;
+    }
+    dispatch({ type: 'SET_ANSWER', payload: currentAnswers });
+  };
+
+  const handleClickNext = () => {
+    setActiveQuestion((prevActiveQuestion) => prevActiveQuestion + 1);
+  };
+
+  const handleClickPrev = () => {
+    setActiveQuestion((prevActiveQuestion) => prevActiveQuestion - 1);
   };
 
   return (
@@ -20,9 +69,45 @@ function TriviaQuiz() {
       <div className="h-48 bg-violet-500 grid items-center justify-center mb-4">
         <h1 className="text-2xl text-white">Trivia Quiz</h1>
       </div>
-      <div className="container mx-auto text-center">
+      <div className="container mx-auto text-center p-2">
         {isStarted ? (
-          <div>Quiz is started</div>
+          <>
+            <Question className="-mt-16 mb-4">
+              {state?.quiz[activeQuestion]?.question}
+            </Question>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
+              {state?.quiz[activeQuestion]?.options?.map((option, idx) => (
+                <Option
+                  key={`option-${idx}`}
+                  id={option}
+                  name="options"
+                  value={option}
+                  onChange={(e) => handleChangeOption(e, activeQuestion)}
+                  checked={option === state?.answers[activeQuestion]}
+                >
+                  {option}
+                </Option>
+              ))}
+            </div>
+            <div className="flex justify-between">
+              <button
+                type="button"
+                className="px-4 py-2 bg-white border rounded-md hover:bg-violet-100 disabled:bg-slate-100 disabled:text-slate-400"
+                onClick={handleClickPrev}
+                disabled={isPrevDisabled}
+              >
+                Prev
+              </button>
+              <button
+                type="button"
+                className="px-4 py-2 bg-white border rounded-md hover:bg-violet-100 disabled:bg-slate-100 disabled:text-slate-400"
+                onClick={handleClickNext}
+                disabled={isNextDisabled}
+              >
+                Next
+              </button>
+            </div>
+          </>
         ) : (
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-2">
             {categories.map((category, idx) => (
