79cd9d4f70506d9f4e177e6e874634e25aa0c03b
rnwathon <rakhmatnashrul@gmail.com>
2022-04-06 19:21:37 +0700

    feat(TriviaQuiz): create shuffle array function

---
+++ src/Pages/9-TriviaQuiz/Utils/shuffleArr.js
@@ -0,0 +1,22 @@
+const shuffleArr = (array) => {
+  const shuffledArray = [...array];
+  let currentIndex = shuffledArray.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
+      shuffledArray[randomIndex],
+      shuffledArray[currentIndex],
+    ];
+  }
+
+  return shuffledArray;
+};
+
+export default shuffleArr;
