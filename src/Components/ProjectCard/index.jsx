d21bb07d6a1447b307bab563d38e7acac8b5f21f
rnwathon <rakhmatnashrul@gmail.com>
2022-04-08 22:59:23 +0700

    feat: create ProjectCard Component

---
+++ src/Components/ProjectCard/index.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProjectCard = ({ title, image, link = '' }) => (
+  <Link to={link}>
+    <div className="p-4 bg-white rounded-lg shadow-md transition hover:shadow-xl">
+      <img
+        src={image}
+        alt={title}
+        className="mb-2 border rounded-lg h-64 w-full object-cover object-top"
+      />
+      <h2 className="text-xl font-bold">{title}</h2>
+    </div>
+  </Link>
+);
+
+export default ProjectCard;
