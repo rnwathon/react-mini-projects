d5815ecc352f63127aa845b0431b3cef1a9067a2
rnwathon <rakhmatnashrul@gmail.com>
2022-04-09 00:19:00 +0700

    feat: integrate ImgWithFallback in ProjectCard component

--- src/Components/ProjectCard/index.jsx
+++ src/Components/ProjectCard/index.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import ImgWithFallback from '../ImgWithFallback';
 
 const ProjectCard = ({ title, image, link = '' }) => (
   <Link to={link}>
     <div className="p-4 bg-white rounded-lg shadow-md transition hover:shadow-xl">
-      <img
-        src={image}
+      <ImgWithFallback
+        src={image.src}
+        fallback={image.fallback}
         alt={title}
         className="mb-2 border rounded-lg h-64 w-full object-cover object-top"
       />
