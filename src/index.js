d3aafb2000fda2767dc80894a8af594844ae9a81
rnwathon <rakhmatnashrul@gmail.com>
2022-05-29 15:33:01 +0700

    feat: add basename to hash router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/">
       <App />
     </HashRouter>
   </React.StrictMode>,
